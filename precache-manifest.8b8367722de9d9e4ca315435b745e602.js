a4fca1c9eaa3e86d81c1adf2c4c89f2ba7fd023f
faizanillahi9 <faizanillahi9@gmail.com>
2020-12-23 14:16:29 +0500

    Updates

---
+++ precache-manifest.8b8367722de9d9e4ca315435b745e602.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d8ed3eb3c2f31610781cb2c3dd8599",
+    "url": "/react-project-1/index.html"
+  },
+  {
+    "revision": "fe5ed4a84a021299f7c7",
+    "url": "/react-project-1/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "843b098c8660ade9540b",
+    "url": "/react-project-1/static/css/main.e0ea3750.chunk.css"
+  },
+  {
+    "revision": "fe5ed4a84a021299f7c7",
+    "url": "/react-project-1/static/js/2.dffb6705.chunk.js"
+  },
+  {
+    "revision": "54e8c4f782a1df2882087d61fdec9005",
+    "url": "/react-project-1/static/js/2.dffb6705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843b098c8660ade9540b",
+    "url": "/react-project-1/static/js/main.8673d00f.chunk.js"
+  },
+  {
+    "revision": "81047495374c8b6b8390",
+    "url": "/react-project-1/static/js/runtime-main.8b2d3d39.js"
+  },
+  {
+    "revision": "0d0ffb53e99c6a76f5fa542296783611",
+    "url": "/react-project-1/static/media/about.0d0ffb53.jpg"
+  },
+  {
+    "revision": "79f40c9d7911921f1f99701d05d27ae0",
+    "url": "/react-project-1/static/media/card-1.79f40c9d.jpg"
+  },
+  {
+    "revision": "c3faebe3e8a55f26b3adce2f7d7d7f3c",
+    "url": "/react-project-1/static/media/card-2.c3faebe3.jpg"
+  },
+  {
+    "revision": "e9a390077e0f83204f9804f113ecd163",
+    "url": "/react-project-1/static/media/card-3.e9a39007.jpg"
+  },
+  {
+    "revision": "d8453a2b79be44b8934293393254ec4c",
+    "url": "/react-project-1/static/media/card-4.d8453a2b.jpg"
+  },
+  {
+    "revision": "a771b8f456d56bf1c34f12c0b0d09df8",
+    "url": "/react-project-1/static/media/card-5.a771b8f4.jpg"
+  },
+  {
+    "revision": "bfcdfe65ec7a55d5f6290f5a7400d17e",
+    "url": "/react-project-1/static/media/card-6.bfcdfe65.jpg"
+  },
+  {
+    "revision": "8ee4fd9768f1716384c40a4cf764fa77",
+    "url": "/react-project-1/static/media/home.8ee4fd97.jpg"
+  }
+]);
